8700a953a5da00843a71da7eab2dcbe2312432d2
ofekgani <ag8339@bs.amalnet.k12.il>
2019-05-30 09:46:19 +0300

    hhh

---
+++ app/src/main/java/com/example/quadraticequation/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.quadraticequation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button btn_next, btn_random;
+    public EditText ed_a, ed_b, ed_c;
+    double a, b, c;
+    String st_a, st_b, st_c;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ed_a = findViewById(R.id.ed_a);
+        ed_b = findViewById(R.id.ed_b);
+        ed_c = findViewById(R.id.ed_c);
+        btn_next = findViewById(R.id.btn_next);
+        btn_random = findViewById(R.id.btn_random);
+    }
+
+    public void random(View view) {
+        Random rnd = new Random();
+        a = rnd.nextInt(100);
+        b = rnd.nextInt(100);
+        c = rnd.nextInt(100);
+        ed_a.setText(""+a);
+        ed_b.setText(""+b);
+        ed_c.setText(""+c);
+    }
+
+    public void next(View view) {
+        st_a = ed_a.getText().toString();
+        a = Double.parseDouble(st_a);
+
+        st_b = ed_b.getText().toString();
+        b = Double.parseDouble(st_b);
+
+        st_c = ed_c.getText().toString();
+        c = Double.parseDouble(st_c);
+    }
+}
